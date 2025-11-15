3dd8351885c6c340244d94c914af8bdfac47d4c9
suhitaAurthi <aurthi2020@gmail.com>
2025-11-15 14:04:52 +0600

    new additions

--- src/cvbuilder/Hello.java
+++ src/cvbuilder/Hello.java
@@ -2,6 +2,9 @@ package cvbuilder;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import  javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 
 public class Hello extends Application {
 
@@ -11,6 +14,16 @@ public class Hello extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+  Label label=new Label("Hello, it's my first project\n");
 
+  StackPane root=new StackPane(label);
+
+  Scene scene=new Scene(root,400,200);
+
+  primaryStage.setScene(scene);
+
+  primaryStage.setTitle("My CVBuilder");
+
+  primaryStage.show();
     }
 }
