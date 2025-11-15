0916e5e5d029cb5f1a0ba7bb5871fbf771656926
Suhita Aurthi <aurthi2020@gmail.com>
2025-11-15 09:52:51 +0600

    tgit add . git commit -m "updated all files" git push

---
+++ src/cvbuilder/Mainn.java
@@ -0,0 +1,16 @@
+package cvbuilder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Mainn extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
