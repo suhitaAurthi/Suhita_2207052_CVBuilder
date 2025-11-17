ba00b469b7103d92126af0b93d3ec3bfbf9eae58
suhitaAurthi <aurthi2020@gmail.com>
2025-11-17 13:40:49 +0600

    new

---
+++ src/main/java/cvbuilder/controllers/CreateCV.java
@@ -0,0 +1,16 @@
+package main.java.cvbuilder.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CreateCV extends Application {
+
+    public  static  void main(String[] args){
+
+    }
+
+    @Override
+    public  void start(Stage arg0) throws  Exception{
+
+    }
+}
