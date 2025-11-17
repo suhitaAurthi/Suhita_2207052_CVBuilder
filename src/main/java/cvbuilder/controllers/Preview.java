1f0609492427a7601e2031ff60ed314ae74b2b0a
suhitaAurthi <aurthi2020@gmail.com>
2025-11-17 13:40:49 +0600

    new

---
+++ src/main/java/cvbuilder/controllers/Preview.java
@@ -0,0 +1,14 @@
+package main.java.cvbuilder.controllers;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class Preview extends Application{
+
+    public  static  void main(String[] args){
+
+    }
+
+    @Override
+    public  void start(Stage arg0) throws  Exception{
+
+    }
+}
