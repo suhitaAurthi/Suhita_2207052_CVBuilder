d214089e1175e10d6b672f902af216334fc46112
suhitaAurthi <aurthi2020@gmail.com>
2025-11-17 13:52:02 +0600

    removed project files

--- src/main/java/cvbuilder/controllers/CreateCVController.java
+++ src/main/java/cvbuilder/controllers/CreateCVController.java
@@ -1,4 +1,16 @@
 package main.java.cvbuilder.controllers;
 
-public class CreateCVController {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CreateCVController extends Application {
+
+    public  static  void main(String[] args){
+
+    }
+
+    @Override
+    public  void start(Stage arg0) throws  Exception{
+
+    }
 }
